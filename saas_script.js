b8631227d4ab6370679d7136ea6b57c45ab04dd4
Marcelo Polidori Colombo <marcelo@MacBook-Pro-de-Marcelo.local>
2023-03-02 22:20:30 -0300

    initial

---
+++ saas_script.js
@@ -0,0 +1,47 @@
+console.log('|||||||||| worked! ||||||||||')
+
+function init () {
+    console.log('|||||||||| init! ||||||||||')
+    
+    const linkCss = document.createElement('link');
+    linkCss.rel  = 'stylesheet';
+    linkCss.type = 'text/css';
+    linkCss.href = 'https://test.vercel.app/saas_styles.css';
+    linkCss.media = 'all';
+    document.head.appendChild(linkCss);
+
+    let divVideoWrapper = document.createElement('div');
+    divVideoWrapper.classList.add("video-wrapper");
+    divVideoWrapper.addEventListener("click", () => {
+        console.log('|||||||||| startIframe! ||||||||||')
+        divVideoWrapper.className = 'display-none';;
+        
+        let iframe = document.createElement('iframe');
+        iframe.src = 'https://test.vercel.app';
+        iframe.id = 'saasVideo';
+        iframe.setAttribute('allow', 'autoplay');
+        iframe.setAttribute('autoplay', 'true');
+        iframe.setAttribute('loop', 'true');
+        iframe.setAttribute('width', '400px');
+        iframe.setAttribute('height', '710px');
+        iframe.setAttribute('frameBorder', '0');
+        document.body.appendChild(iframe);
+    });
+    document.body.appendChild(divVideoWrapper);
+
+
+
+    let video = document.createElement('video');
+    video.src = 'https://test.vercel.app/videotest.mp4';
+    video.id = 'saasVideo';
+    iframe.setAttribute('allow', 'autoplay');
+    video.setAttribute('autoplay', 'true');
+    video.setAttribute('loop', 'true');
+    video.setAttribute('width', '200px');
+    video.setAttribute('height', '305px');
+    divVideoWrapper.appendChild(video);
+    
+
+}
+
+init();
